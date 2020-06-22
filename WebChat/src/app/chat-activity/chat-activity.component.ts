43b980445c7d4ca048f96b649712399fb7f26d79
duong17020661 <duong.coi.bs.1104@gmail.com>
2020-06-22 23:27:06 +0700

    Them chat-toolbar và chat-activity

--- WebChat/src/app/chat-activity/chat-activity.component.ts
+++ WebChat/src/app/chat-activity/chat-activity.component.ts
@@ -6,7 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat-activity.component.css']
 })
 export class ChatActivityComponent implements OnInit {
-
+  public sender = [{
+    message: "Terrance Orta",
+}, {
+    message: "Richard Mahoney",
+}, {
+    message: "Donna Price",
+}, {
+    message: "Lisa Landers",
+}, {
+    message: "Dorothy H. Spencer",
+}];
+  public receiver = [{
+  message: "Terrance Orta",
+}, {
+  message: "Richard Mahoney",
+}, {
+  message: "Donna Price",
+}, {
+  message: "Lisa Landers",
+}, {
+  message: "Dorothy H. Spencer",
+}];
   constructor() { }
 
   ngOnInit(): void {
